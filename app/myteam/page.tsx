f5ba29cf7c721875a3ca1b0d23157a6cea54b3cd
Francisco Martinez Gallardo <franmgl2001@gmail.com>
2025-10-31 09:36:56 +0100

    Fix errors is closed

--- app/myteam/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function MyTeamPage() {
-    return (
-        <main className="p-4 pb-20">
-            <h1 className="text-xl font-semibold">My Team</h1>
-        </main>
-    );
-}
-
-
