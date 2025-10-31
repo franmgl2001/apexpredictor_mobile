8090001cfb08a75b98434a73874bbc9dacf4aa8e
Francisco Martinez Gallardo <franmgl2001@gmail.com>
2025-10-31 15:46:47 +0100

    Add random thing for loginh

--- mobile_app/index.js
+++ mobile_app/index.js
@@ -2,6 +2,12 @@
  * @format
  */
 
+// Import and initialize Amplify React Native modules first to ensure native modules are registered
+import { loadGetRandomValues } from '@aws-amplify/react-native';
+
+// Load required native modules for Amplify
+loadGetRandomValues();
+
 import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
